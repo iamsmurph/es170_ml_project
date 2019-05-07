2137929461e65a36f8c15e25a0fae4f2c1f42a08
Phil Labrum <plabrum@college.harvard.edu>
2019-05-06 21:47:27 -0400

    implementation of iris

--- ml/pyquil_dbc.py
+++ ml/pyquil_dbc.py
@@ -5,49 +5,28 @@ import numpy as np
 class PQ_DistanceBasedClassifier:
 
     def create_circuit(self, angles):
-        """
-        Creating the quantum circuit
-        by filling in the gaps with the
-        defined `angles` that are required
-        to load the test and training vectors.
-        """
 
         p = Program()
         ro = p.declare("ro", memory_type='BIT', memory_size=4)
 
         p += H(0)
         p += H(1)
-
-        def u3(program, theta, qubit):
-            program += RZ(3*np.pi, qubit)
-            program += RX(np.pi/2, qubit)
-            program += RZ(theta+np.pi, qubit)
-            program += RX(np.pi/2, qubit)
-            program += RZ(0, qubit)
-            return program
-
         p += CNOT(0, 2)
-        #p = u3(p, -angles[0], 2)
         p += RY(-angles[0], 2)
         p += CNOT(0, 2)
-        #p = u3(p, angles[0], 2)
         p += RY(angles[0], 2)
         p += X(0)
         p += CCNOT(0, 1, 2)
         p += X(1)
         p += CCNOT(0, 1, 2)
         p += CNOT(1, 2)
-        #p = u3(p, angles[1], 2)
         p += RY(angles[1], 2)
         p += CNOT(1, 2)
-        #p = u3(p, -angles[1], 2)
         p += RY(-angles[1], 2)
         p += CCNOT(0, 1, 2)
         p += CNOT(1, 2)
-        #p = u3(p, -angles[1], 2)
         p += RY(-angles[1], 2)
         p += CNOT(1, 2)
-        #p = u3(p, angles[1], 2)
         p += RY(-angles[1], 2)
         p += CNOT(1, 3)
         p += H(0)
@@ -61,25 +40,6 @@ class PQ_DistanceBasedClassifier:
         return p
 
     def simulate(self, program):
-        """
-        Compile and run the quantum circuit
-        on a simulator backend.
-        """
-        # Create quantum computer (simulation)
-        qc = get_qc('4q-noisy-qvm')
-
-        # perhaps another way to do it locally?
-        # qc = get_qc(QPU_LATTICE_NAME, as_qvm=True)
-
-        #shown somewhere as a different way
-        #qvm = QVMconnection()
-
-        # Measure the qubits specified by classical_register (qubits 0 and 1) a number of times
-        program.wrap_in_numshots_loop(shots=20)
-
-        comp = qc.compile(program)
-        # Compile and run the Program (returns a 20 element array of arrays of qubit results)
-        results = qc.run(comp)
 
         def sort(arr):
           (uni, counts) = np.unique(arr, axis=0, return_counts = True)
@@ -89,43 +49,18 @@ class PQ_DistanceBasedClassifier:
           final = dict(zip(a, counts))
           return final
 
-        # retrieve the results from the simulation
+        qc = get_qc('4q-noisy-qvm')
+
+        program.wrap_in_numshots_loop(shots=20)
+        comp = qc.compile(program)
+        results = qc.run(comp)
+
         return sort(results)
-
-    def get_angles(self, test_vector, training_vectors):
-        """
-        Return the angles associated with
-        the `test_vector` and the `training_vectors`.
-        Note: if you want to extend this classifier
-        for other test and training vectors you need to
-        specify the angles here!
-        """
-        angles = []
-
-        if test_vector == [-0.549, 0.836]:
-            angles.append(4.30417579487669/2)
-        elif test_vector == [0.053 , 0.999]:
-            angles.append(3.0357101997648965/2)
-        else:
-            print('No angle defined for this test vector.')
-
-        if training_vectors[0] == [0, 1] and training_vectors[1] == [0.78861006, 0.61489363]:
-            angles.append(1.3245021469658966/4)
-        else:
-            print('No angles defined for these training vectors.')
-
-        return angles
 
     def get_angle_for_vec(self, vec):
         return float(np.arccos(vec[0]))*2.0
 
     def interpret_results(self, result_counts):
-        """
-        Post-selecting only the results where
-        the ancilla was measured in the |0> state.
-        Then computing the statistics of the class
-        qubit.
-        """
 
         total_samples = sum(result_counts.values())
 
@@ -136,43 +71,25 @@ class PQ_DistanceBasedClassifier:
         postselection = dict(post_select(result_counts))
         postselected_samples = sum(postselection.values())
 
-        print(f'Ancilla post-selection probability was found to be {postselected_samples/total_samples}')
+        #print(f'Ancilla post-selection probability was found to be {postselected_samples/total_samples}')
 
         retrieve_class = lambda binary_class: [occurences for state, occurences in postselection.items() if state[0] == str(binary_class)]
 
         prob_class0 = sum(retrieve_class(0))/postselected_samples
         prob_class1 = sum(retrieve_class(1))/postselected_samples
 
-        print(f'Probability for class 0 is {prob_class0}')
-        print(f'Probability for class 1 is {prob_class1}')
+        #print(f'Probability for class 0 is {prob_class0}')
+        #print(f'Probability for class 1 is {prob_class1}')
 
         return prob_class0, prob_class1
 
     def classify(self, test_vector, training_set):
-        """
-        Classifies the `test_vector` with the
-        distance-based classifier using the `training_vectors`
-        as the training set.
-        This functions combines all other functions of this class
-        in order to execute the quantum classification.
-        """
 
-        # extract training vectors
         training_vectors = [tuple_[0] for tuple_ in training_set]
-
-
-        # get the angles needed to load the data into the quantum state
-
-
-        angles1 = self.get_angles(
-                test_vector=test_vector,
-                training_vectors=training_vectors
-        )
 
         angles = []
         angles.append(self.get_angle_for_vec(test_vector)/2)
         angles.append(self.get_angle_for_vec(training_vectors[1])/4)
-        print(angles)
 
         # create the quantum circuit
         program = self.create_circuit(angles=angles)
--- ml/run.py
+++ ml/run.py
@@ -1,9 +1,14 @@
 import qiskit_dbc as QK
 import pyquil_dbc as PQ
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler, normalize
+import pandas as pd
+import numpy as np
+import seaborn as sns; sns.set()
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    # initiate an instance of the distance-based classifier
     QK_classifier = QK.QK_DistanceBasedClassifier()
     PQ_classifier = PQ.PQ_DistanceBasedClassifier()
 
@@ -16,6 +21,38 @@ if __name__ == "__main__":
         ([0.78861006, 0.61489363], 1) # class 1 training vector
     ]
 
+    def feed_the_classifier(dataset, classifier):
+        results = []
+        for vec in dataset:
+            cl = classifier.classify(vec, training_set)
+            vec.append(cl)
+            results.append(vec)
+        return results
+
+    # print([x_prime])
+    # res1 = feed_the_classifier([x_prime], PQ_classifier)
+    # print(res1)
+
+    iris_data = load_iris().data[:3,:2]
+    data = iris_data
+    scalar = StandardScaler()
+    scalar.fit(data)
+    data1 = scalar.transform(data)
+    data2 = np.ndarray.tolist(normalize(data1))
+
+    res = np.asarray(feed_the_classifier(data2, PQ_classifier))
+    print(res)
+
+    for el in res[:, 2]:
+
+
+    df  = pd.DataFrame(res, columns = ['x', 'y', 'label'])
+    print(df)
+    # df.plot()  # plots all columns against index
+    sns.relplot(x="x", y="y", hue="label", data=df);
+    # df.plot(kind='density')  # estimate density function
+    plt.show()
+    '''
     print("--------------------- QisKit version ----------------------")
     print(f"Classifying x' = {x_prime} with noisy simulator backend")
     class_result = QK_classifier.classify(test_vector=x_prime, training_set=training_set)
@@ -38,3 +75,5 @@ if __name__ == "__main__":
     print(f"Classifying x'' = {x_double_prime} with noisy simulator backend")
     class_result = PQ_classifier.classify(test_vector=x_double_prime, training_set=training_set)
     print(f"Test vector x' was classified as class {class_result}")
+    '''
+
